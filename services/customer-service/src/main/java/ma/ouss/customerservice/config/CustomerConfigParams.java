033dd37ec43752c776766b5136ba440e95e62897
ouss-issib <oussamaissib@gmail.com>
2025-11-21 13:05:43 +0100

    feat : use refreshScope to auto refresh params inside config service

---
+++ services/customer-service/src/main/java/ma/ouss/customerservice/config/CustomerConfigParams.java
@@ -0,0 +1,9 @@
+package ma.ouss.customerservice.config;
+
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "customer.params")
+public record CustomerConfigParams(int x,int y) {
+
+}
