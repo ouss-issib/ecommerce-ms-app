195c98b5e745a3e1cff6ac816a624a7d66bbdf89
ouss-issib <oussamaissib@gmail.com>
2025-11-27 21:39:31 +0100

    feat : create angular-client app

---
+++ angular-client/frontent-ecomerce-ms-app/src/app/app.config.ts
@@ -0,0 +1,14 @@
+import { ApplicationConfig, provideBrowserGlobalErrorListeners,importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { routes } from './app.routes';
+import { HttpClientModule } from '@angular/common/http';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideRouter(routes),
+    provideAnimations(),
+    importProvidersFrom(HttpClientModule)
+  ]
+};
